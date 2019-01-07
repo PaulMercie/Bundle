9f3d4a36fd4382cf43218d2735c3d6239400fe70
PaulMercie <paulmercie453@gmail.com>
2019-01-07 12:25:44 +0100

    :tada: init webpack

---
+++ bundler/webpack.config.js
@@ -0,0 +1,11 @@
+// Librairie qui permet de générer les chemins
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output:
+    {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist')
+    }
+}
