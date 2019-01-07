13d3bf58ace4f2f4460da0692dec20909e4d28bf
PaulMercie <paulmercie453@gmail.com>
2019-01-07 17:52:20 +0100

    :memo: Adding docs on index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+// Import de tous les différents dossiers/fichiers 
 import './css/style.css'
 import imageSource from './images/image.jpg'
 import Carousel from './js/Carousel.js'
 
+// Changement de la source de l'image
 const image = new Image()
 image.src = imageSource
 document.body.appendChild(image)
